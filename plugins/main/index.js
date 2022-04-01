5357813f7cabe9d90b5446d6568ae1159a17cc4d
uEthel_Yunyize <yunyize@yunyize.com>
2022-04-01 03:26:13 +0000

    修复main程序换行显示问题

--- plugins/main/index.js
+++ plugins/main/index.js
@@ -3,7 +3,7 @@ const { unit } = require('../../module');
 module.exports = opt => {
     return async (wsc, data) => {
         if (data.post_type === 'message') {
-            unit.log(`收到消息：'${data.message.replace(/\n/g, '')}'，消息类型：${data.message_type === 'private' ? '私聊' : '群'}消息，发送人：${data.user_id}，发送时间：${unit.formatTime(new Date(data.time * 1000))}`,'MainPlugin');
+            unit.log(`收到消息：'${data.message.replace(/\n/g, ' ')}'，消息类型：${data.message_type === 'private' ? '私聊' : '群'}消息，发送人：${data.user_id}，发送时间：${unit.formatTime(new Date(data.time * 1000))}`,'MainPlugin');
         }
     }
 }
